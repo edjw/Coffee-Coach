6bc6ee8e4d74982e1450a8b07c553f2539fb15b4
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2020-09-05 13:48:31 +0100

    styling and copy

--- src/routes/v60-by-james-hoffman.svelte
+++ src/routes/v60-by-james-hoffman.svelte
@@ -20,13 +20,15 @@
 
 <style>
   section {
-    margin-top: 2rem;
+    margin-top: 3rem;
+    max-width: 60ch;
   }
 
   h2 {
+    display: inline;
     border-top: 1px solid #333;
     border-bottom: 1px solid #333;
-    padding: 14px 0 16px 0;
+    padding: 14px 16px 16px 0;
   }
 </style>
 
@@ -39,9 +41,9 @@
 <section>
   <h3>Ingredients</h3>
 
-  {#if $currentCalculator == 'coffeeFirst'}
+  {#if $currentCalculator == "coffeeFirst"}
     <CoffeeFirst on:togglecalculator={toggleCalculator} />
-  {:else if $currentCalculator == 'waterFirst'}
+  {:else if $currentCalculator == "waterFirst"}
     <WaterFirst on:togglecalculator={toggleCalculator} />
   {/if}
 </section>
@@ -53,7 +55,12 @@
 </section>
 
 <section>
-  This is a recipe simplified from <a
-    href="https://www.youtube.com/watch?v=AI4ynXzkSQo">James Hoffman's video '<i>The
-      Ultimate V60 Technique</i></a>.'
+  <h3>Notes</h3>
+  <p>
+    This is a recipe simplified from <a
+      href="https://www.youtube.com/watch?v=AI4ynXzkSQo">James Hoffman's video '<i>The
+        Ultimate V60 Technique</i></a>.'
+  </p>
+
+  <p>It uses a ratio of 30g coffee to 500g water (1 to 16.67).</p>
 </section>
