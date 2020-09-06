66fc2cca16231f05f53542aa4d63bad22be96ff3
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2020-09-06 22:48:53 +0100

    removing non-persistent store

--- src/components/v60/v60store.js
+++ src/components/v60/v60store.js
@@ -8,12 +8,4 @@ let coffeeAmount = writable("coffeeAmount", 15);
 
 let currentCalculator = writable("currentCalculator", "coffeeFirst")
 
-// import { writable } from 'svelte/store';
-
-// let waterAsMultipleOfCoffee = writable(16.66666666666667);
-// let waterAmount = writable(250);
-// let coffeeAmount = writable(15);
-
-// let currentCalculator = writable("coffeeFirst")
-
 export { waterAmount, coffeeAmount, waterAsMultipleOfCoffee, currentCalculator };
