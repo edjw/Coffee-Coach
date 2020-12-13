3df7415bc6c0e3740b539788c2054eac03b12804
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2020-12-13 22:43:31 +0000

    some nonsense apostrophe formatting

--- src/routes/_allRecipes.js
+++ src/routes/_allRecipes.js
@@ -3,7 +3,6 @@ const pages = [
     title: "v60 by James Hoffman",
     slug: "v60-by-james-hoffman",
   },
-
   {
     title: "French Press by James Hoffman",
     slug: "french-press-by-james-hoffman",
